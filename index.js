8cb347448d31321b11dda0e8e1765430aebdd0bf
farzd <qasimf@tcd.ie>
2017-05-14 22:12:29 +0100

    DB model definitions

--- index.js
+++ index.js
@@ -19,3 +19,66 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+
+
+  const User = sequelize.define('users', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    name: {
+      type: Sequelize.STRING
+    },
+    username: {
+      type: Sequelize.STRING
+    },
+    facebookID: {
+      type: Sequelize.STRING
+    },
+    description: {
+      type: Sequelize.STRING
+    }
+  });
+
+  const Stories = sequelize.define('stories', {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    date: {
+      type: Sequelize.DATE
+    },
+    url: {
+      type: Sequelize.STRING,
+    }
+  });
+
+User.hasMany(Stories)
+Stories.belongsTo(User)
+
+sequelize.sync();
+
+//  User.create({
+//     name: 'Davids',
+//     facebookID: '001',
+//     description: 'robot model #1'
+//   });
+
+  // Stories.create({
+  //   userId: 4,
+  //   date: new Date(),
+  //   url: '/aws/some2.jpg'
+  // });
+
+
+User.findAll({
+  include: [{
+    model: Stories
+  }]
+}).then((s) => {
+  console.log(s)
+}).catch((s) => {
+  console.log(s)
+})
