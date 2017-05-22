0c72c6dcd58f4328432feac28bb1b61abdb403af
farzd <qasimf@tcd.ie>
2017-05-22 22:06:08 +0100

    Register user if it doesnt exists, use facebookID from graphAPI

--- routes/login.js
+++ routes/login.js
@@ -4,21 +4,52 @@ var router = express.Router();
 const { sequelize: { models } } = require('../models/index.js');
 const  { jwt: {secret} } = require('../config');
    
-router.get('/:facebookID', function (req, res, next) {
+//shouldnt use facebook as param, just use what the facebook API returns 
+function facebookGraphCall() {
+    //use auth token to get facebookID
+    return '001';
+}
+
+function createJWToken(res, id) {
+    const user = {
+        id
+    };
+    const token = jwt.sign(user, secret);
+    return res.json({
+        token
+    });
+}
+
+router.get('/', function (req, res, next) {
+    const facebookID = facebookGraphCall();
     models.users.find({
         where: {
-            facebookID: req.params.facebookID
+            facebookID: facebookID
         },
-       raw: true
-    }).then((result) => {
-        const user = {
-            id: result.id
-        };
-        const token = jwt.sign(user, secret);
-        return res.json({token});
+        raw: true
+    }).then((result) => { 
+        if (result) {
+            return createJWToken(res, result.id);
+        }
+        return models.users.create({
+            name: 'Rippley',
+            username: 'ripley005',
+            facebookID: facebookID,
+            description: 'robot model #5',
+            followers: [1, 2, 3]
+        }).then((newUser) => {
+            const newUserId = newUser.get({plain: true}).id;
+            return createJWToken(res, newUserId);
+        }).catch((err) => {
+            return next({
+                msg: `unable to create user with facebookID ${facebookID}`,
+                statusCode: 500,
+                err
+            });            
+        });
     }).catch((err) => {
         return next({
-            msg: `unable to query user with id ${req.params.facebookID}`,
+            msg: `unable to query user with id ${facebookID}`,
             statusCode: 500,
             err
         });
