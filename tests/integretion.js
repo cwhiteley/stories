0b05d71566f967ecac3329beb97077a984280741
farzd <qasimf@tcd.ie>
2017-06-04 19:44:26 +0100

    Test for user update details mutation

--- tests/integretion.js
+++ tests/integretion.js
@@ -13,7 +13,7 @@ describe('Integration Tests', function() {
                 return {
                     name: 'Rippley',
                     username: 'ripley005',
-                    facebookID: '001',
+                    facebookID: '000',
                     description: 'robot model #5'
                 }
             }
@@ -38,7 +38,7 @@ describe('Integration Tests', function() {
     });
 
     describe('Login', function () {
-        it('should generate a JWT token', function(done) {
+        it('should find or create mocked user and generate a JWT token', function(done) {
             server
             .get('/login')
             .then(res => {
@@ -50,15 +50,52 @@ describe('Integration Tests', function() {
     });
 
     describe('Users', function() {
-        it('should return a user', function(done) {
+        
+        before(function() {
+            db.sequelize.models.users.bulkCreate([{
+                name: 'David',
+                username: 'david001',
+                facebookID: '001',
+                description: 'robot model #1'
+            }, {
+                name: 'Walter',
+                username: 'walter001',
+                facebookID: '002',
+                description: 'robot model #2'
+            }, {
+                name: 'Elizabeth',
+                username: 'liz',
+                facebookID: '003',
+                description: 'shes real'
+            }]);
+        });
+        
+        it('Query: should return a user', function(done) {
             server
                 .post('/graphql?token=' + token)
-                .send({"query": "query {user(id: 1) {facebookID,username,name,description}}"})
+                .send({'query': 'query {user(id: 1) {facebookID,username,name,description}}'})
                 .expect(200)
                 .end((err, res) => {
-                    assert.equal(res.body.data.user.name, 'Rippley')
+                    assert.equal(res.body.data.user.name, 'Rippley');
+                    assert.equal(res.body.data.user.username, 'ripley005');
+                    assert.equal(res.body.data.user.facebookID, '000');
+                    assert.equal(res.body.data.user.description, 'robot model #5');
                     done();
                 })
+        });
+
+        it('Mutation: should let you update user details', function(done) {
+            server
+                .post('/graphql?token=' + token)
+                .send({'query': 'mutation {userUpdateDetails(userId: 1, name:"FakeRipley", desc:"whatever", username:"ripleyclone22") {facebookID,username,name,description}}'})
+                .expect(200)
+                .end((err, res) => {
+
+                    assert.equal(res.body.data.userUpdateDetails.name, 'FakeRipley');
+                    assert.equal(res.body.data.userUpdateDetails.username, 'ripleyclone22');
+                    assert.equal(res.body.data.userUpdateDetails.description, 'whatever');
+                    done();
+                })            
         });
 
     });
