3e62f033759a4cd2f37021daa53b2da60bfc7f3e
farzd <qasimf@tcd.ie>
2017-06-08 22:08:20 +0100

    Refactor tests to use setup

---
+++ tests/integretion/setup.js
@@ -0,0 +1,47 @@
+var proxyquire =  require('proxyquire');
+const path = require('path');
+var db = require('../../src/models');
+
+module.exports = function() {
+    const facebookStub = {
+            init: function () {
+                return {
+                    name: 'Rippley',
+                    username: 'ripley005',
+                    facebookID: '000',
+                    description: 'robot model #5'
+                }
+            }
+        };
+
+        proxyquire(path.join(__dirname, '../', '../', 'routes', 'login.js'), {
+            '../src/utils/facebook': facebookStub
+        });
+
+        return db.connect({force: true}).then(()=> {
+            return populate();
+        }).catch((e) => {
+            console.log('Error connecting to DB/populating ', e);
+            return Promise.reject();
+        });
+
+        function populate() {
+            return db.sequelize.models.users.bulkCreate([{
+                name: 'David',
+                username: 'david001',
+                facebookID: '001',
+                description: 'robot model #1'
+            }, {
+                name: 'Walter',
+                username: 'walter001',
+                facebookID: '002',
+                description: 'robot model #2'
+            }, {
+                name: 'Elizabeth',
+                username: 'liz',
+                facebookID: '003',
+                description: 'shes real'
+            }])     
+        }
+    
+}
--- tests/integretion/users.js
+++ tests/integretion/users.js
@@ -1,65 +1,17 @@
-const request = require('supertest');
-var proxyquire =  require('proxyquire');
 const path = require('path');
 const assert = require('assert');
-var db = require('../../src/models');
+const request = require('supertest');
+const setup = require('./setup');
 let app, server, token;
 
 describe('Integration Tests', function() {
 
     beforeEach(function(done) {
-        const facebookStub = {
-            init: function () {
-                return {
-                    name: 'Rippley',
-                    username: 'ripley005',
-                    facebookID: '000',
-                    description: 'robot model #5'
-                }
-            }
-        };
-
-
-        proxyquire(path.join(__dirname, '../', '../', 'routes', 'login.js'), {
-            '../src/utils/facebook': facebookStub
-        });
-
-        db.connect({force: true}).then(()=> {
-            return populate().then(()=> {
-                return connectServer().then(() => {
-                    done();
-                });
-            });
-        }).catch((e) => {
-            console.log('Error connecting to DB/populating ', e);
-            done();
-        });
-
-        function populate() {
-            return db.sequelize.models.users.bulkCreate([{
-                name: 'David',
-                username: 'david001',
-                facebookID: '001',
-                description: 'robot model #1'
-            }, {
-                name: 'Walter',
-                username: 'walter001',
-                facebookID: '002',
-                description: 'robot model #2'
-            }, {
-                name: 'Elizabeth',
-                username: 'liz',
-                facebookID: '003',
-                description: 'shes real'
-            }])     
-        }
-
-        function connectServer() {
+        setup().then(() => {
             app = require(path.join(__dirname, '../', '../', 'server.js'));
             server = request(app);
-            return Promise.resolve();
-        }
-
+            done();
+        });
     });
 
     describe('Login', function () {
